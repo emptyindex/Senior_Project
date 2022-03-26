ec2319b44d553d11dbf2058511e08e5dd34c07e8
Alex Henson <ahenson4@students.kennesaw.edu>
2022-03-25 20:52:01 -0400

    removed excess LevelLoader

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
-
     public Animator transition;
 
     // Update is called once per frame
     void Update()
     {
-         if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0))
         {
             LoadNextLevel();
         }
@@ -22,12 +21,12 @@ public class LevelLoader : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
 
-    IEnumerator LoadLevel(int levelIndex)
-    {
-        //play animation
+    //IEnumerator LoadLevel(int levelIndex)
+    //{
+    //    //play animation
 
-        //wait
+    //    //wait
 
         
-    }
+    //}
 }
